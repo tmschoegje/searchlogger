e000c5737ef9f7ed142fc258348e700c5b788d09
Thomas Schoegje <tmschoegje@gmail.com>
2019-02-12 09:02:36 +0100

    signed up google billing, initial parsing of oris results

--- search/google-custom-search-master/search-script.js
+++ search/google-custom-search-master/search-script.js
@@ -40,6 +40,7 @@ function Search(term, engine, direction)
 		+ mGoogleApiKey + "&num=" + _resultsPerPage + "&cx=" + mGoogleCustomSearchKey + "&start=" + startIndex + "&q=" + escape(term) + "&callback=?";
 
  //   url = "http://hahndorf/ws/dummy.aspx?q=" + escape(term) + "&start=" + startIndex + "&callback=?";
+		_engine = "g"
 	}
 	//oris
 	else{
@@ -69,68 +70,93 @@ function getPreview(str, keywords){
 	console.log(chunks);
 }
 
-function SearchCompleted(response, keywords)
+function parseiBabs(events, keywords){
+	console.log('iBABS')
+	results = []
+	for(i = 0; i < events.length; i++){
+		newresult = {
+			"title": events[i].classification + " " + events[i].name,
+			"documents": [{}]
+		}
+		for(j = 0; j < events[i].sources.length; j++){
+			newresult.documents.push({
+				"title": events[i].sources[j].note,
+				//"preview": getPreview(events[i].sources[j].description, keywords),
+				"url": events[i].sources[j].url
+			})
+		}
+		results.push(newresult)
+//		console.log(newresult)
+	}
+	console.log(results)
+}
+
+function SearchCompleted(response)
 {
-	getPreview("Hello, how is it going. This is the bus we have to take!", _keywords);
+	//getPreview("Hello, how is it going. This is the bus we have to take!", _keywords);
 	console.log('search completed')
 	console.log(_engine)
 	console.log(response)
-	
-	
-	result1 = response.events[0].classification + " " + response.events[0].name
-	console.log(result1)
-	
-    var html = "";
-    $("#searchResult").html("");
+		
+	if(_engine == "g"){
+		var html = "";
+		$("#searchResult").html("");
 
-    if (response.items == null)
-    {
-        $("#searchResult").html("No matching pages found");
-        return;
-    }
+		if (response.items == null)
+		{
+			$("#searchResult").html("No matching pages found");
+			return;
+		}
 
-    if (response.items.length === 0)
-    {
-        $("#searchResult").html("No matching pages found");
-        return;
-    }
+		if (response.items.length === 0)
+		{
+			$("#searchResult").html("No matching pages found");
+			return;
+		}
 
-    $("#searchResult").html(response.queries.request[0].totalResults + " pages found for <b>" +response.queries.request[0].searchTerms+ "</b><br><br>");
+		$("#searchResult").html(response.queries.request[0].totalResults + " pages found for <b>" +response.queries.request[0].searchTerms+ "</b><br><br>");
 
-    if (response.queries.nextPage != null)
-    {
-        _nextIndex = response.queries.nextPage[0].startIndex;
-        $("#lnkNext").show();
-    }
-    else
-    {
-        $("#lnkNext").hide();
-    }
+		if (response.queries.nextPage != null)
+		{
+			_nextIndex = response.queries.nextPage[0].startIndex;
+			$("#lnkNext").show();
+		}
+		else
+		{
+			$("#lnkNext").hide();
+		}
 
-    if (response.queries.previousPage != null)
-    {
-        _prevIndex = response.queries.previousPage[0].startIndex;
-        $("#lnkPrev").show();
-    }
-    else
-    {
-        $("#lnkPrev").hide();
-    }
+		if (response.queries.previousPage != null)
+		{
+			_prevIndex = response.queries.previousPage[0].startIndex;
+			$("#lnkPrev").show();
+		}
+		else
+		{
+			$("#lnkPrev").hide();
+		}
 
-    if (response.queries.request[0].totalResults > _resultsPerPage){
-        $("#lblPageNumber").show().html(_pageNumber);
-    }
-    else{
-        $("#lblPageNumber").hide();
-    }
+		if (response.queries.request[0].totalResults > _resultsPerPage){
+			$("#lblPageNumber").show().html(_pageNumber);
+		}
+		else{
+			$("#lblPageNumber").hide();
+		}
 
-    for (var i = 0; i < response.items.length; i++){
-        var item = response.items[i];
-        var title = item.htmlTitle;
+		for (var i = 0; i < response.items.length; i++){
+			var item = response.items[i];
+			var title = item.htmlTitle;
         
-        html += "<p><a class='searchLink' href='" + item.link + "'> " + title + "</a><br>";
-        html += item.snippet + "<br>";
-        html += item.link + " - <a href='http://www.google.com/search?q=cache:"+item.cacheId+":"+item.displayLink+"'>Cached</a></p><p>";
-    }
-    $("#output").html(html);
+			html += "<p><a class='searchLink' href='" + item.link + "'> " + title + "</a><br>";
+			html += item.snippet + "<br>";
+			html += item.link + " - <a href='http://www.google.com/search?q=cache:"+	item.cacheId+":"+item.displayLink+"'>Cached</a></p><p>";
+		}
+	}
+	else{
+		parseiBabs(response.events, _keywords)
+
+		html = ""
+		
+	}
+	$("#output").html(html)
 }
